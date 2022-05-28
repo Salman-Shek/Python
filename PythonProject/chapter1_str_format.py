f57af6b30ed37a87eb94a2238fbc4c498c86eef9
Salman-Shek <salman2sms@gmail.com>
2022-05-29 01:47:15 +0530

    python

---
+++ PythonProject/chapter1_str_format.py
@@ -0,0 +1,14 @@
+name = "Jennel"
+school = "JSS Public"
+print(name + school)
+print(name + " " +school)
+print("{} works at {} School".format(name,school))
+
+name ="Salman"
+Age=32
+
+print(name + str(Age))
+print(name + " is " +str(Age) +" years old")
+print("{} is {} years old".format(name,Age))
+print("%s is %s years old" % (name,Age))
+print(f'{name} is {Age} years old')
---
+++ PythonProject/chapter1_variables.py
@@ -0,0 +1,21 @@
+# its a single line comment
+# prg on variables
+
+cars = 23
+CARS = 24
+_Cars = 25
+Number_Of_Cars = 26
+kind_of_cars ="Ferrari"
+
+print(cars)
+print(CARS)
+print(_Cars)
+print(Number_Of_Cars)
+print(kind_of_cars)
+
+cars =27
+print(cars)
+
+''' This is multi
+line comment
+'''
